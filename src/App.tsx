373558d5e15239418432e2d7c926c0f814961bfa
Ganesh-Paudel <paudelganesh080@gmail.com>
2026-01-22 12:52:36 -0500

    First Commit with a react project. It works so far

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+	  <>
+		<h1> This is a title </h1>
+	  </>
+  )
+}
+
+
+function Profile() {
+  return (
+    <img
+      src="https://i.imgur.com/QIrZWGIs.jpg"
+      alt="Alan L. Hart"
+    />
+  );
+}
+
